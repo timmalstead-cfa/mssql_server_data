968d84f293791d5d24148de58bd40d4ce4ec7b21
timmalstead-cfa <tmalstead@codeforamerica.org>
2021-06-11 14:54:41 -0700

    creates checks for route parameters

--- dist/index.js
+++ dist/index.js
@@ -15,104 +15,132 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const sequelize_1 = require("sequelize");
 const dbSetup_1 = __importDefault(require("./dbSetup"));
+const constants_1 = require("./constants");
 const server = express_1.default();
 let models;
-// TODO: add checks for route parameters. I don't think that there's too much danger for injection, but why risk it?
 server.get("/getbycategory", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const { orgObj, locObj, servObj } = models;
         const { category, language } = req.query;
-        const returnedOrgs = yield orgObj.findAll({
-            where: { [`categories_${language}`]: { [sequelize_1.Op.like]: `%${category}%` } },
-            attributes: [
-                "id",
-                `categories_${language}`,
-                `name_${language}`,
-                `tags_${language}`,
-            ],
-            include: [
-                {
-                    model: locObj,
-                    required: false,
-                    attributes: ["latitude", "longitude", "city"],
-                    through: { attributes: [] },
-                    include: [
-                        {
-                            model: servObj,
-                            required: false,
-                            attributes: [`name_${language}`],
-                            through: { attributes: [] },
-                        },
-                    ],
-                },
-            ],
-            order: [[`name_${language}`, "ASC"]],
-        });
-        res.json(returnedOrgs);
+        if (constants_1.categories.has(category) &&
+            constants_1.languages.has(language)) {
+            const { orgObj, locObj, servObj } = models;
+            const returnedOrgs = yield orgObj.findAll({
+                where: { [`categories_${language}`]: { [sequelize_1.Op.like]: `%${category}%` } },
+                attributes: [
+                    "id",
+                    `categories_${language}`,
+                    `name_${language}`,
+                    `tags_${language}`,
+                ],
+                include: [
+                    {
+                        model: locObj,
+                        required: false,
+                        attributes: ["latitude", "longitude", "city"],
+                        through: { attributes: [] },
+                        include: [
+                            {
+                                model: servObj,
+                                required: false,
+                                attributes: [`name_${language}`],
+                                through: { attributes: [] },
+                            },
+                        ],
+                    },
+                ],
+                order: [[`name_${language}`, "ASC"]],
+            });
+            res.json(returnedOrgs);
+        }
+        else {
+            throw new Error("language or category parameter is not valid");
+        }
+    }
+    catch (error) {
+        console.error(error.message);
+        res.json(error.message);
+    }
+}));
+server.get("/getsinglerecord", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { id, language } = req.query;
+        if (constants_1.languages.has(language)) {
+            const { orgObj, locObj, servObj, schObj } = models;
+            const returnedOrg = yield orgObj.findOne({
+                where: { id },
+                attributes: [
+                    "id",
+                    `name_${language}`,
+                    "website",
+                    `languages_spoken_${language}`,
+                    `customers_served_${language}`,
+                    `notes_${language}`,
+                    `tags_${language}`,
+                ],
+                include: [
+                    {
+                        model: locObj,
+                        required: false,
+                        through: { attributes: [] },
+                        include: [
+                            {
+                                model: servObj,
+                                required: false,
+                                attributes: ["id", `name_${language}`],
+                                through: { attributes: [] },
+                            },
+                            {
+                                model: schObj,
+                                required: false,
+                                through: { attributes: [] },
+                            },
+                        ],
+                    },
+                ],
+            });
+            res.json(returnedOrg);
+        }
+        else {
+            throw new Error("language parameter is not valid");
+        }
     }
     catch (error) {
         console.error(error);
         res.json(error);
     }
 }));
-server.get("/getsinglerecord", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { orgObj, locObj, servObj, schObj } = models;
-    const { id, language } = req.query;
-    const returnedOrg = yield orgObj.findOne({
-        where: { id },
-        attributes: [
-            "id",
-            `name_${language}`,
-            "website",
-            `languages_spoken_${language}`,
-            `customers_served_${language}`,
-            `notes_${language}`,
-            `tags_${language}`,
-        ],
-        include: [
-            {
-                model: locObj,
-                required: false,
-                through: { attributes: [] },
+server.get("/searchbykeyword", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { query, language } = req.query;
+        if (constants_1.languages.has(language)) {
+            const { orgObj, locObj } = models;
+            const returnedOrgs = yield orgObj.findAll({
+                where: { [`tags_${language}`]: { [sequelize_1.Op.like]: `%${query}%` } },
+                attributes: [
+                    "id",
+                    `categories_${language}`,
+                    `name_${language}`,
+                    `tags_${language}`,
+                ],
                 include: [
                     {
-                        model: servObj,
-                        required: false,
-                        attributes: ["id", `name_${language}`],
-                        through: { attributes: [] },
-                    },
-                    {
-                        model: schObj,
-                        required: false,
+                        model: locObj,
+                        attributes: ["latitude", "longitude", "city"],
                         through: { attributes: [] },
                     },
                 ],
-            },
-        ],
-    });
-    res.json(returnedOrg);
-}));
-server.get("/searchbykeyword", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { orgObj, locObj } = models;
-    const { query, language } = req.query;
-    const returnedOrgs = yield orgObj.findAll({
-        where: { [`tags_${language}`]: { [sequelize_1.Op.like]: `%${query}%` } },
-        attributes: [
-            "id",
-            `categories_${language}`,
-            `name_${language}`,
-            `tags_${language}`,
-        ],
-        include: [
-            {
-                model: locObj,
-                attributes: ["latitude", "longitude", "city"],
-                through: { attributes: [] },
-            },
-        ],
-        order: [[`name_${language}`, "ASC"]],
-    });
-    res.json(returnedOrgs);
+                order: [[`name_${language}`, "ASC"]],
+            });
+            res.json(returnedOrgs);
+        }
+        else {
+            throw new Error("language parameter is not valid");
+        }
+    }
+    catch (error) {
+        console.error(error);
+        res.json(error);
+    }
 }));
 server.listen(8000, () => {
     console.log(`Express server up and running`);
